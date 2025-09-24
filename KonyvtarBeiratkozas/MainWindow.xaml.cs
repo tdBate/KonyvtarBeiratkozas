4bf73549081cf7bff60b85a382bd8ccb1ea4475f
TorokD <td.zazsa@gmail.com>
2025-09-24 10:54:05 +0200

    Add project files.

---
+++ KonyvtarBeiratkozas/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KonyvtarBeiratkozas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string[] mufajok = { "regény", "scifi", "fantasy", "ismeretterjesztő", "mese"};
+        public MainWindow()
+        {
+            InitializeComponent();
+            cmbMufaj.ItemsSource = mufajok;
+        }
+    }
+}
